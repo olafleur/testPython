f28e8edae80f8496735b40aee1095cb097d1a8ad
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-01-30 11:25:01 -0500

    Nouveau test

--- tests.py
+++ tests.py
@@ -5,6 +5,9 @@ class DesTests(unittest.TestCase):
     def test(self):
         self.assertTrue(True)
 
+    def autreTest(self):
+        self.assertEquals(3, 3)
+
 
 if __name__ == '__main__':
     unittest.main()

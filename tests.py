6dcf00da10e8e338da61987ca66e8f0ce9486fcd
Olivier Lafleur <Olivier.Lafleur@cll.qc.ca>
2014-01-31 21:05:47 -0500

    tests teamcity

--- tests.py
+++ tests.py
@@ -1,14 +1,33 @@
+# coding=utf-8
+from teamcity import is_running_under_teamcity
+from teamcity.unittestpy import TeamcityTestRunner
+
 import unittest
 
 
-class DesTests(unittest.TestCase):
-    #test22222
-    def test(self):
-        self.assertTrue(True)
+class TestTeamcityMessages(unittest.TestCase):
+    def testPass(self):
+        assert 1 == 1
 
-    def autre_test(self):
-        self.assertEquals(3, 3)
+    def testAssertEqual(self):
+        self.assertEqual("1", "2")
+
+    def testAssertEqualMsg(self):
+        self.assertEqual("1", "2", "message")
+
+    def testAssert(self):
+        self.assert_(False)
+
+    def testFail(self):
+        self.fail("failed")
+
+    def testException(self):
+        raise Exception("some exception")
 
 
 if __name__ == '__main__':
-    unittest.main()
+    if is_running_under_teamcity():
+        runner = TeamcityTestRunner()
+    else:
+        runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
